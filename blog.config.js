96b72d8dcad0bd98a788032babb55a764061c160
yuanrui12315 <247028490+yuanrui12315@users.noreply.github.com>
2025-12-18 12:04:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -10,7 +10,7 @@ const BLOG = {
 
   NOTION_TOKEN: 'ntn_41625482344bcsP6cEHGha5GxV6b2YtTmTjPYneE1ItG38E',
 
-  THEME: 'commerce', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
+  THEME: 'example', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
 
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US' see /lib/lang.js for more.
 
