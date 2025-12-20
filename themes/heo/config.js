dee8924d45d8a87ae2111ccf87b8dd6ebec9346c
yuanrui12315 <247028490+yuanrui12315@users.noreply.github.com>
2025-12-20 23:08:50 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -117,10 +117,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-//  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
+  HEO_SOCIAL_CARD_TITLE_1: '加我电报✈️✈️',
  // HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加我电报✈️✈️',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://t.me/@wy6691888',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
