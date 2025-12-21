3dfddf186548af7677e784df805c8ed33972bf44
yuanrui12315 <247028490+yuanrui12315@users.noreply.github.com>
2025-12-21 22:15:35 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -60,7 +60,7 @@ const CONFIG = {
   HEO_GROUP_ICONS: [
     {
       title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
+      img_1: '/images/heo/nse-2450786388214037294-1765588709986.jpg.jpg',
       color_1: '#989bf8',
       title_2: 'Sketch',
       img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
