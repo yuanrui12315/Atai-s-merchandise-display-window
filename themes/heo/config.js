e96e85ca9d1564d875f055ca925153a11b37133a
yuanrui12315 <247028490+yuanrui12315@users.noreply.github.com>
2025-12-22 00:16:13 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -24,10 +24,10 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'ATAIWU888.COM',
   HEO_HERO_TITLE_4: '分类浏览',
   HEO_HERO_TITLE_5: '阿泰小店商品目录',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_LINK: 'https://www.ataiwu888.com/',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '诚信第一',
-
+ HEO_HERO_RIGHT: false,
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
