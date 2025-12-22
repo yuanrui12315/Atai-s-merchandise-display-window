8f104078423a2876b8763995170e6f535ed82177
yuanrui12315 <247028490+yuanrui12315@users.noreply.github.com>
2025-12-23 04:31:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -63,7 +63,7 @@ const BLOG = {
 
   // RSS订阅
 
-  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
+  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || false, // 是否开启RSS订阅功能
 
   // 其它复杂配置
 
