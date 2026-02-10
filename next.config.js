18b6592f36edc8f7a39b9861eb51d6086cdb4971
yuanrui12315 <247028490+yuanrui12315@users.noreply.github.com>
2026-02-10 22:43:10 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
@@ -8,6 +10,9 @@ const nextConfig = {
     esmExternals: 'loose'
   },
   webpack: (config) => {
+    // 补回路径映射，解决 @theme-components 找不到的问题
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    config.resolve.alias['@theme-components'] = path.resolve(__dirname, 'themes')
     config.resolve.fallback = { ...config.resolve.fallback, fs: false };
     return config;
   }
