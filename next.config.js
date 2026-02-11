c5406fff4b6806d4bcd8a5635f37e38ee829d733
yuanrui12315 <247028490+yuanrui12315@users.noreply.github.com>
2026-02-11 19:45:23 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-const path = require('path')
-
 module.exports = {
   output: 'standalone',
   images: { unoptimized: true },
   webpack: (config) => {
-    // 官方别名：这才是最稳的寻路方式
-    config.resolve.alias['@theme-components'] = path.resolve(__dirname, 'themes/theme.js')
-    config.resolve.alias['@'] = path.resolve(__dirname)
     config.resolve.fallback = { ...config.resolve.fallback, fs: false }
     return config
   }
